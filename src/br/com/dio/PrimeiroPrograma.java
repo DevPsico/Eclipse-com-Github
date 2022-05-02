f6f0de8b27ba223c2b69b2461f0d429dc4084710
DevPsico <ericsonsergio19844@gmail.com>
2022-05-02 18:53:00 -0300

    Primeiro Commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,29 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Gato gato = new Gato();
+		Livros livros = new Livros();
+
+		System.out.println(gato);
+		System.out.println(livros);
+
+		/*
+		 * int a = 5; int b = 8;
+		 * 
+		 * System.out.println("Olá Mundo !!!! " +(a+b));
+		 */
+
+	}
+
+}
+
+class Livros {
+	private String nome;
+	private String npag;
+}
